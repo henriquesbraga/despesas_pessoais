cb0ef6a8f9de3642cfb095970cfd6de29073fc95
henriquesbraga <Henri@1996>
2023-06-29 09:51:47 -0300

    persistence branch

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -12,53 +12,56 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-            ? Column(
-                children: [
-                  Text(
-                    "Nenhuma Transação cadastrada!",
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                      height: 200,
-                      child: Image.asset(
-                        'assets/images/waiting.png',
-                        fit: BoxFit.cover,
-                      ))
-                ],
-              )
-            : ListView.builder(
-                itemCount: transactions.length,
-                itemBuilder: (ctx, index) {
-                  final tr = transactions[index];
-                  return Card(
-                    elevation: 5,
-                    margin:
-                        const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                          radius: 30,
-                          child: Padding(
-                            padding: EdgeInsets.all(6),
-                            child: FittedBox(
-                              child: Text('R\$${tr.value}'),
-                            ),
-                          )),
-                      title: Text(
-                        tr.title,
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                      subtitle: Text(
-                        DateFormat('dd/MM/yyyy hh:m:ss').format(tr.date),
-                        style: const TextStyle(color: Colors.grey),
-                      ),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () { onRemove(tr.id);},
-                        color: Theme.of(context).colorScheme.error,
-                      ),
+      ? Column(
+        children: [
+          Text(
+            "Nenhuma Transação cadastrada!",
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            height: 200,
+            child: Image.asset(
+              'assets/images/waiting.png',
+              fit: BoxFit.cover,
+            )
+          )
+        ],
+      )
+      : ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (ctx, index) {
+            final tr = transactions[index];
+            return Card(
+              elevation: 5,
+              margin:
+                  const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Padding(
+                    padding: EdgeInsets.all(6),
+                    child: FittedBox(
+                      child: Text('R\$${tr.value}'),
                     ),
-                  );
-                },);
+                  )
+                ),
+                title: Text(
+                  tr.title,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                subtitle: Text(
+                  DateFormat('dd/MM/yyyy hh:m:ss').format(tr.date),
+                  style: const TextStyle(color: Colors.grey),
+                ),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () { onRemove(tr.id);},
+                  color: Theme.of(context).colorScheme.error,
+                ),
+              ),
+            );
+          },
+    );
   }
 }
